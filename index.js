3ca2755523f17fd59631fd33935e58c15c51f3b4
Kratos <adewaleolorunosebi@gmail.com>
2025-07-11 13:59:25 +0100

    firstcommit

---
+++ index.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const Product = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/api/products", productRoute);
+
+app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Node API Server Updated");
+});
+
+
+mongoose.connect("mongodb+srv://adewaleolorunosebi:Adewale@1122@simple-crud-app.ve7gyx4.mongodb.net/Node-API?retryWrites=true&w=majority&appName=simple-crud-app")
+.then(() => {
+    console.log("Connected to MongoDB!");
+})
+.catch(() => {
+        console.log("Connection failed");
+    });
